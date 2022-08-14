ac324beca7f9b40350b7decb66acf4511b761964
Ernst Thelifort <ethelifort@gmail.com>
2022-08-14 11:07:20 -0400

    Keyboard added without full functionality

--- script.js
+++ script.js
@@ -17,16 +17,70 @@ function initBoard(){
 
      
     for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
-        let row = document.createElement("div")
+        let row = document.createElement("div") //Creating the rows.
         row.className = "letter-row"
         
         for (let j = 0; j < 5; j++) {
-            let box = document.createElement("div")
+            let box = document.createElement("div") //Putting the boxes on the rows.
             box.className = "letter-box"
-            row.appendChild(box)
+            row.appendChild(box) 
         }
 
-        board.appendChild(row)
+        board.appendChild(row) //Adding the rows to the board.
     }
 }
 
+initBoard();
+
+document.addEventListener("keyup,", (e)=>{
+
+    if(guessesRemaining === 0){
+        return;
+    }
+
+    let pressedKey = String(e.key);
+    if(pressedKey === "Backspace" && nextLetter !==0){
+        deleteLetter();
+        return;
+    }
+
+    if(pressedKey === "Enter"){
+        checkGuess();
+        return;
+    }
+
+    let found = pressedKey.match(/[a-z]/gi);//Reg 
+    if(!found || found.length > 1){
+            return;
+    } else{
+        insertLetter(pressedKey); 
+    }
+})
+
+function insertLetter(pressedKey){
+
+    if(nextLetter === 5){
+        return;
+    }
+
+    pressedKey = pressedKey.toLowerCase();
+
+    let row = document.getElementsByClassName('letter-row')[6-guessesRemaining]; //Check were not at the end of a row.
+    let box = row.children[nextLetter];//Figure out the box on the row where it needs to go.
+    box.textContent = pressedKey;
+
+    box.classList.add("filled-box");
+    currentGuess.push(pressedKey); // Add the pressed key.
+    nextLetter += 1;
+    
+}
+
+function deleteLetter(){
+    let row =  document.getElementsByClassName('letter-row')[6 - guessesRemaining];
+    let box =  row.children[nextLetter-1]
+    box.textContent = '';
+    box.classList.remove('filled-box');
+    currentGuess.pop()
+
+    nextLetter -=1;
+}
